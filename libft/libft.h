e83bca2bc35aea908e62481cab4ffcf213c3eaf3
jsaldana <saldana.juan.2002@gmail.com>
2022-08-30 19:57:37 +0200

    More thngs ft_bzero

--- libft/libft.h
+++ libft/libft.h
@@ -6,12 +6,12 @@
 /*   By: jsaldana <jsaldana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 19:43:10 by jsaldana          #+#    #+#             */
-/*   Updated: 2022/08/17 19:43:10 by jsaldana         ###   ########.fr       */
+/*   Updated: 2022/08/30 19:46:20 by jsaldana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -19,5 +19,31 @@
 # include <string.h>
 # include <fcntl.h>
 
+/**
+ * @brief The first array takes the value of the first n bytes of the second
+ * array.
+ * If the first array and the second are null, returns null.
+ * 
+ * inside the function, I create a counter that works while is lower than the
+ * passed bytes.
+ * 
+ * @note ¿Do I need an aux var?
+ * @note ¿Can I change letters by numbers?
+ * 
+ * @param origin 
+ * @param destination 
+ * @param bytesToCopy 
+ * @return void* 
+ */
 void	*ft_memcpy(const void *origin, void *destination, size_t bytesToCopy);
+
+/**
+ * @brief deprecated, use better @memset(3)
+ * This function erases or set to zero the first n bytes of the pointer given.
+ * 
+ * @param s pointer
+ * @param n number of bytes to erase.
+ * 
+ */
+void	ft_bzero(void *s, size_t n);
 #endif
