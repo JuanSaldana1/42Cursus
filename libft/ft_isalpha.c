afc42ee4f3a2fbcac45fe14c9131117e92e6b8fd
jsaldana <saldana.juan.2002@gmail.com>
2022-08-31 19:04:57 +0200

    ft_isalpha finished

---
+++ libft/ft_isalpha.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsaldana <jsaldana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 18:55:46 by jsaldana          #+#    #+#             */
+/*   Updated: 2022/08/31 19:04:17 by jsaldana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (0);
+	else
+		return (1);
+}
+
+/* int main(int argc, char const *argv[])
+{
+	char	chain[] = "123AbG";
+	int		count;
+
+	count = 0;
+	while (chain[count])
+	{
+		printf("%d\n", ft_isalpha(chain[count]));
+		count++;
+	}
+	return (0);
+} */
