0c50cea747e2f8a9c72fba47d2a721b4393990e8
jsaldana <saldana.juan.2002@gmail.com>
2022-09-02 12:16:07 +0200

    OK ft_isAlpha, ft_isAlNum && ft_strchr

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -15,9 +15,9 @@
 int	ft_isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-		return (0);
-	else
 		return (1);
+	else
+		return (0);
 }
 
 /* int main(int argc, char const *argv[])
--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -15,9 +15,9 @@
 int	ft_isascii(int c)
 {
 	if (c >= 0 && c <= 127)
-		return (0);
-	else
 		return (1);
+	else
+		return (0);
 }
 
 /* int	main(int argc, char const *argv[])
