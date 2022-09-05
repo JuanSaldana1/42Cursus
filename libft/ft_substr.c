c60a796311bd3abaf6a26a567f8da7333cf7b88c
jsaldana <saldana.juan.2002@gmail.com>
2022-09-05 12:46:10 +0200

    ft_substr.c DONE!!

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jsaldana <jsaldana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 11:56:24 by jsaldana          #+#    #+#             */
-/*   Updated: 2022/09/05 12:07:41 by jsaldana         ###   ########.fr       */
+/*   Updated: 2022/09/05 12:41:39 by jsaldana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	int		count;
 
 	count = 0;
-	str = "";
-	while (s[start++] != '\0' && len > 0)
+	if (start >= ft_strlen(s))
 	{
-		str[count] = s[start];
-		count++;
-		len--;
+		str = malloc(1);
+		str[0] = 0;
+		return (str);
 	}
+	if (len >= ft_strlen(s))
+		len = ft_strlen(s);
+	str = (char *) malloc(len * sizeof (char) + 1);
+	if (!str || !s)
+		return (NULL);
+	while (s[start + count] != '\0' && count < (int)len)
+	{
+		str[count] = s[start + count];
+		count++;
+	}
+	str[count] = '\0';
 	return (str);
 }
