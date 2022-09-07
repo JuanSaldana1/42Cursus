5f011cb84bc59f15f23a051a9124ad66fa614280
jsaldana <saldana.juan.2002@gmail.com>
2022-09-07 16:52:45 +0200

    improved ft_strncmp.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jsaldana <jsaldana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 11:14:58 by jsaldana          #+#    #+#             */
-/*   Updated: 2022/09/06 11:15:39 by jsaldana         ###   ########.fr       */
+/*   Updated: 2022/09/07 16:48:26 by jsaldana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
 	unsigned int	count;
-	int				result;
 
 	count = 0;
-	while (count < (n - 1))
+	while (count < (n - 1) && str1 != NULL && str2 != NULL)
 	{
-		if (ft_strlen(str1) == ft_strlen(str2))
-			result = (0);
-		else if (ft_strlen(str1) > ft_strlen(str2))
-			result = ((int)(ft_strlen(str1) - ft_strlen(str2)));
-		else if (ft_strlen(str2) > ft_strlen(str1))
-			result = ((int)(ft_strlen(str2) - ft_strlen(str1)));
+		if (str1[count] != str2[count])
+		{
+			if (str1[count] > str2[count])
+				return ((str1[count]) - str2[count]);
+			else if (str2[count] > str1[count])
+				return ((str2[count]) - str1[count]);
+		}
 		count++;
 	}
-	return (result);
+	return (0);
 }
