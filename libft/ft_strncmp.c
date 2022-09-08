3240087f86069f7a98611d83948b983cfc17f6e4
jsaldana <saldana.juan.2002@gmail.com>
2022-09-08 12:06:17 +0200

    ft_strncmp.c WORKS OK

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,27 +6,38 @@
 /*   By: jsaldana <jsaldana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 11:14:58 by jsaldana          #+#    #+#             */
-/*   Updated: 2022/09/07 16:55:14 by jsaldana         ###   ########.fr       */
+/*   Updated: 2022/09/08 12:04:08 by jsaldana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
 
 int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
-	unsigned int	count;
+	int				count;
+	unsigned char	*s1;
+	unsigned char	*s2;
 
+	s1 = (unsigned char *)str1;
+	s2 = (unsigned char *)str2;
 	count = 0;
-	while (count < (n - 1) && ft_strlen(str1) != 0 && ft_strlen(str2) != 0)
+	while ((str1[count] != '\0' || str2[count] != '\0') && count < (int)n)
 	{
-		if (str1[count] != str2[count])
+		if (s1[count] != s2[count])
 		{
-			if (str1[count] > str2[count])
-				return ((str1[count]) - str2[count]);
-			else if (str2[count] > str1[count])
-				return ((str2[count]) - str1[count]);
+			if (s1[count] > s2[count])
+				return (s1[count] - s2[count]);
+			else if (s2[count] > s1[count])
+				return (s1[count] - s2[count]);
 		}
 		count++;
 	}
 	return (0);
 }
+/*
+int main (void)
+{
+	printf("%d\n", ft_strncmp("test", "testss", 7));
+}
+*/
