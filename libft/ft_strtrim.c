76aeb43f4b640294596a166c1ae32a61e42d117a
jsaldana <saldana.juan.2002@gmail.com>
2022-09-11 14:42:49 +0200

    Finished ft_strtrim.c

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,45 +6,57 @@
 /*   By: jsaldana <jsaldana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 12:10:32 by jsaldana          #+#    #+#             */
-/*   Updated: 2022/09/11 14:22:58 by jsaldana         ###   ########.fr       */
+/*   Updated: 2022/09/11 14:29:50 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+/**
+ * @brief Checks if the given string has the char passed as parameter
+ * @param c
+ * @param set
+ * @return
+ */
+static size_t		ft_check_char(char c, char const *set)
 {
-	char	*result;
-	int		count;
-	int		i;
-	size_t	end;
+	size_t		i;
 
-	end = ft_strlen(s1);
-	count = 0;
 	i = 0;
-	result = "";
-	if (s1[end -1] == set[0])
+	while (set[i])
 	{
-		end--;
+		if (set[i] == c)
+			return (1);
+		i++;
 	}
-	else
-	{
-		result[count] = s1[end -1];
-		count ++;
-	}
-	while (s1[count] == set[0])
-	{
-		count ++;
-	}
-
-	while (s1[count] < s1[end])
-		{
-			result[i] = s1[count];
-			i++;
-			count++;
-		}
-	return (result);
+	return (0);
 }
+
+char		*ft_strtrim(char const *s1, char const *set)
+{
+	char		*str;
+	size_t		i;
+	size_t		start;
+	size_t		end;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_check_char(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_check_char(s1[end - 1], set))
+		end--;
+	str = (char *)malloc(sizeof(*s1) * (end - start + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		str[i++] = s1[start++];
+	str[i] = 0;
+	return (str);
+}
+
 
 int	main(void)
 {
