92c6dd0205f3eab7555820dfbf1271078753a96b
jsaldana <saldana.juan.2002@gmail.com>
2022-09-13 17:37:49 +0200

    ft_memchr.c DONE

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: jsaldana <jsaldana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 12:13:31 by jsaldana          #+#    #+#             */
-/*   Updated: 2022/09/13 12:29:14 by jsaldana         ###   ########.fr       */
+/*   Updated: 2022/09/13 17:32:51 by jsaldana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,16 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	count;
-	char	*str;
+	size_t			count;
+	unsigned char	*str;
 
 	count = 0;
-	str = s;
-	while (str[count] < n)
+	str = (unsigned char *)s;
+	while (count < n)
 	{
-		if (str[count] == c)
-			return ((void *)(count));
-		else
-			return (NULL);
+		if (str[count] == (unsigned char)c)
+			return (str + count);
 		count++;
 	}
+	return (0);
 }
