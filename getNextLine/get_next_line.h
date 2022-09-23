ea6dcb636e7370382df858563bb213da3488dc66
jsaldana <saldana.juan.2002@gmail.com>
2022-09-23 10:15:51 +0200

    Starting projects getNextLine && printf

---
+++ getNextLine/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsaldana <jsaldana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/23 10:00:00 by jsaldana          #+#    #+#             */
+/*   Updated: 2022/09/23 10:00:00 by jsaldana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+//
+// Created by Juan Saldana Rived on 9/23/22.
+//
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+/**
+ * @brief
+ * @param fd
+ * @return The read line of the file descriptor
+ */
+char	*get_next_line(int fd);
+
+#endif //GET_NEXT_LINE_H
