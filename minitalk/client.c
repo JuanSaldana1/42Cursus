6ad5ed231106fe1e2db25b26ce3e2a8c0fb97563
jsaldana <saldana.juan.2002@gmail.com>
2022-11-30 19:37:12 +0100

    More Avances

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,18 +6,30 @@
 /*   By: jsaldana <jsaldana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 10:27:17 by                   #+#    #+#             */
-/*   Updated: 2022/11/05 14:26:23 by jsaldana         ###   ########.fr       */
+/*   Updated: 2022/11/30 19:36:49 by jsaldana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-
+#include <stdio.h>
+void showbits (int pid, unsigned char n)
+{
+	int	i = 8;
+	while (i--)
+	{
+		if (n >> i & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+	}
+	printf("\n");
+}
 int	main(int argc, const char *argv[])
 {
 	if (argc == 3)
 	{
-		kill(ft_atoi(argv[1]), SIGUSR2);
+		showbits(ft_atoi(argv[1]), 'a');
+		printf("my pid is %d\n", getpid());
 	}
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,26 +6,44 @@
 /*   By: jsaldana <jsaldana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 10:31:31 by                   #+#    #+#             */
-/*   Updated: 2022/11/05 14:00:15 by jsaldana         ###   ########.fr       */
+/*   Updated: 2022/11/30 19:30:45 by jsaldana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void sig_handler(int signum)
+void sig_handler(int signum, siginfo_t *info, void *context)
 {
-  printf("\nSERVER RECIEVED SIGNAL (%d)\n", signum);
+	(void)info;
+	(void)context;
+	
+	
+	if (signum == SIGUSR1)
+	{
+		printf("1");
+		// printf("\nSERVER RECIEVED SIGUSR1 from: (%d)\n", info->si_pid);
+		// printf("%s\n", (char *)info->si_value.sival_ptr);
+		// printf("%d\n", info->si_value.sival_int);
+		// printf("%d\n", info->si_code);
+		// printf("%s\n", context);
+	}
+	if (signum == SIGUSR2)
+		printf("0");
+		// printf("\nSERVER RECIEVED SIGUSR2 (%d)\n", signum);
+
 }
 
 int	main(void)
 {
-	int	pid;
-	// struct sigaction	s_sigaction;
+	int					pid;
+	struct sigaction	sig_action;
 
 	pid = getpid();
 	ft_putnbr(pid);
-	signal(SIGUSR1, sig_handler);
-	// s_sigaction.sa_flags = SA_SIGINFO;
+	sig_action.sa_flags = SA_SIGINFO;
+	sig_action.sa_sigaction = sig_handler;
+	sigaction(SIGUSR1, &sig_action, NULL);
+	sigaction(SIGUSR2, &sig_action, NULL);
 	while (1)
 		sleep(1);
 	return (0);
